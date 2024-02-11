09a17c685c32646c21dc03fda9ac43e0f04cd40f
Sebchav <schavarria13@alumnos.uaq.mx>
2024-02-10 18:59:17 -0600

    Login NativeTask

--- views/Login.js
+++ views/Login.js
@@ -4,15 +4,35 @@ import { Container, Button, Text, Input, FormControl, Stack } from "native-base"
 
 const Login = () => {
   return (
-    <Container>
         <View>
-            <FormControl>
-                    <Input 
-                        placeholder='email'
-                    />
-            </FormControl>
+          <Text textAlign={"center"} marginTop={3} fontSize="3xl">
+            NativeTask
+          </Text>
+          <FormControl >
+              <Stack space={5}>
+                <Stack marginX={5}>
+                  <FormControl.Label>Email</FormControl.Label>
+                  <Input variant="underlined" p={2} placeholder="Email" type='email'/>
+                </Stack>
+                <Stack marginX={5}>
+                  <FormControl.Label>Password</FormControl.Label>
+                  <Input variant="underlined" p={2} placeholder="Password" type='password'/>
+                </Stack>
+              </Stack>
+           </FormControl>
+
+            <Button
+              square 
+              block
+              marginX={5}
+              marginTop={5}
+            >
+              <Text>Iniciar Sesión</Text>
+            </Button>
+
+         <Text textAlign={"center"} marginTop={3}>Crear Cuenta</Text>
+        
         </View>
-    </Container>
   )
 }
 
